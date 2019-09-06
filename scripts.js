f459dfd971fb567c3ec7806d408ab0c4bb9dd0c0
Felix Maldonado <felix_maldonado@hotmail.com>
2019-09-05 21:23:42 -0400

    Working File

--- scripts.js
+++ scripts.js
@@ -10,17 +10,33 @@
 let searchedTerm = '';
 const searchString = 'https://api.github.com/users/'; // ${someUserHere}/repos
 
+function gitApiCall(searchTerm){
+    fetch('https://api.github.com/users/' + searchTerm + '/repos')
+    .then(response => response.Json)
+    .then(responseJson => display(responseJson));
+    // display(responseJson);
+}
 
-
-
-
+function display(responseJson){
+    for(let i =0; i < responseJson.length; i++){
+        // console.log(responseJson[i].name);
+        $('body').append(`<ul>
+            <li>Name: ${responseJson[i].name}</li>
+            <li>URL:  ${responseJson[i].html_url}</li>
+        </ul>`
+        )
+    };
+}
 
 function callOnSubmit() {
   $('form').submit(function(e) {
     e.preventDefault();
     const searchTerm = $('input').val();
+    gitApiCall(searchTerm);
+    // display(responseJson);
     console.log(searchTerm);
   });
+//   display(responseJson);
 }
 
 $(callOnSubmit);
